df4a2e407e1a9148693f378969196b53bcc2ec4a
hrodmn <henry.rodman@gmail.com>
2024-10-22 11:23:51 -0500

    bring back env var approach for postgres

--- cdk/runtimes/titiler-pgstac-api/src/handler.py
+++ cdk/runtimes/titiler-pgstac-api/src/handler.py
@@ -5,6 +5,23 @@ Handler for AWS Lambda.
 import asyncio
 import os
 
+from utils import get_secret_dict
+
+# Update postgres env variables before importing titiler.pgstac.main
+pgstac_secret_arn = os.environ["PGSTAC_SECRET_ARN"]
+
+secret = get_secret_dict(pgstac_secret_arn)
+os.environ.update(
+    {
+        "postgres_host": secret["host"],
+        "postgres_dbname": secret["dbname"],
+        "postgres_user": secret["username"],
+        "postgres_pass": secret["password"],
+        "postgres_port": str(secret["port"]),
+    }
+)
+
+
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
 from mangum import Mangum
@@ -18,17 +35,13 @@ from titiler.extensions import (
 from titiler.pgstac.db import connect_to_db  # noqa: E402
 from titiler.pgstac.main import app  # noqa: E402
 
-from .settings import ApiSettings
-from .factory import MosaicTilerFactory
-
-
-api_settings = ApiSettings()
+from factory import MosaicTilerFactory
 
 
 @app.on_event("startup")
 async def startup_event() -> None:
     """Connect to database on startup."""
-    await connect_to_db(app, settings=api_settings.postgres)
+    await connect_to_db(app)
 
 
 ########################################
